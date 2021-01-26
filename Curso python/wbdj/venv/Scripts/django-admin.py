3b2788bb781a4a061150c7ca43e2681f94cc005e
Lucas Henrique Calheiros Saraiva Santos <lucashenriquecss@gmail.com>
2021-01-26 20:14:54 -0300

    att

---
+++ Curso python/wbdj/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\calhe\desktop\linguagens\python\curso python\wbdj\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
